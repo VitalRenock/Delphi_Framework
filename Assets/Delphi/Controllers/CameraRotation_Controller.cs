04fb02fdd2dd1642b92669df3905488dc9c7912f
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-07-24 02:28:47 +0200

    Update Controllers

---
+++ Assets/Delphi/Controllers/CameraRotation_Controller.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+[RequireComponent(typeof(Camera))]
+public class CameraRotation_Controller : MonoBehaviour
+{
+    public Space SpaceRotation = Space.Self;
+
+    [HorizontalGroup("Enable")]
+    [BoxGroup("Enable/Can Rotate")] [HideLabel]
+    public bool CanRotate = true;
+    [BoxGroup("Enable/Rotate on X")] [HideLabel]
+    public bool CanRotateOnX = true;
+    [BoxGroup("Enable/Rotate on Y")] [HideLabel]
+    public bool CanRotateOnY = false;
+    [BoxGroup("Enable/Rotate on Z")] [HideLabel]
+    public bool CanRotateOnZ = false;
+
+    [BoxGroup("CanRotateOnX/Rotation X")] [ShowIfGroup("CanRotateOnX")]
+    public string XRotationAxis = "Mouse Y";
+    [BoxGroup("CanRotateOnX/Rotation X")] [ShowIfGroup("CanRotateOnX")]
+    public float SpeedOnX = 120f;
+    [BoxGroup("CanRotateOnX/Rotation X")] [ShowIfGroup("CanRotateOnX")] [Range(0, 180)]
+    public float MaxAngleOnX = 45.0f;
+
+    [BoxGroup("CanRotateOnY/Rotation Y")] [ShowIfGroup("CanRotateOnY")]
+    public string YRotationAxis = "Mouse X";
+    [BoxGroup("CanRotateOnY/Rotation Y")] [ShowIfGroup("CanRotateOnY")]
+    public float SpeedOnY = 120f;
+    [BoxGroup("CanRotateOnY/Rotation Y")] [ShowIfGroup("CanRotateOnY")] [Range(0, 180)]
+    public float MaxAngleOnY = 45.0f;
+
+    [BoxGroup("CanRotateOnZ/Rotation Z")] [ShowIfGroup("CanRotateOnZ")]
+    public string ZRotationAxis = "Mouse ScrollWheel";
+    [BoxGroup("CanRotateOnZ/Rotation Z")] [ShowIfGroup("CanRotateOnZ")]
+    public float SpeedOnZ = 120f;
+    [BoxGroup("CanRotateOnZ/Rotation Z")] [ShowIfGroup("CanRotateOnZ")] [Range(0, 180)]
+    public float MaxAngleOnZ = 45.0f;
+
+    Quaternion cameraRotation;
+
+    private void Start()
+    {
+        if (SpaceRotation == Space.Self)
+            cameraRotation = transform.localRotation;
+        else
+            cameraRotation = transform.rotation;
+    }
+
+    private void Update()
+    {
+        if (!CanRotate)
+            return;
+
+        if (CanRotateOnX)
+            cameraRotation.x = ComputeSingleRotation(cameraRotation.x, -Input.GetAxis(XRotationAxis), SpeedOnX, MaxAngleOnX);
+        if (CanRotateOnY)
+            cameraRotation.y = ComputeSingleRotation(cameraRotation.y, Input.GetAxis(YRotationAxis), SpeedOnY, MaxAngleOnY);
+        if (CanRotateOnZ)
+            cameraRotation.z = ComputeSingleRotation(cameraRotation.z, Input.GetAxis(ZRotationAxis), SpeedOnZ, MaxAngleOnZ);
+
+        if (SpaceRotation == Space.Self)
+            transform.localRotation = Quaternion.Euler(cameraRotation.x, cameraRotation.y, cameraRotation.z);
+        else
+            transform.rotation = Quaternion.Euler(cameraRotation.x, cameraRotation.y, cameraRotation.z);
+    }
+
+    // Clamp un quaternion.
+    float ComputeSingleRotation(float targetRotation, float axisValue, float speed, float maxAngle)
+    {
+        targetRotation += axisValue * speed * Time.deltaTime;
+        targetRotation = Mathf.Clamp(targetRotation, -maxAngle, maxAngle);
+        return targetRotation;
+    }
+}
---
+++ Assets/Delphi/Controllers/FPS_Controller.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class FPS_Controller : MonoBehaviour
+{
+    public bool CanMove = true;
+
+    [TabGroup("Axis")]
+    public string TranslateAxis = "Vertical";
+    [TabGroup("Axis")]
+    public string StrafeAxis = "Horizontal";
+    [TabGroup("Axis")]
+    public string YRotationAxis = "Mouse X";
+    [TabGroup("Axis")]
+    public KeyCode RunKey = KeyCode.LeftShift;
+    [TabGroup("Axis")]
+    public KeyCode JumpKey = KeyCode.Space;
+
+    [TabGroup("Speeds")]
+    public float WalkingSpeed = 7.5f;
+    [TabGroup("Speeds")]
+    public float RunningSpeed = 11.5f;
+    [TabGroup("Speeds")]
+    public float RotationSpeed = 2.0f;
+    [TabGroup("Speeds")]
+    public float JumpSpeed = 8.0f;
+    [TabGroup("Speeds")]
+    public float Gravity = 20.0f;
+
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+
+
+    void Start() => characterController = GetComponent<CharacterController>();
+
+    void Update()
+    {
+        // Calculate player direction for translation.
+        ComputeTranslateDirection();
+
+        // Player translation.
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player rotation.
+        if (CanMove)
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * RotationSpeed, 0);
+    }
+
+    void ComputeTranslateDirection()
+    {
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(RunKey);
+
+        float curSpeedX = CanMove ? (isRunning ? RunningSpeed : WalkingSpeed) * Input.GetAxis(TranslateAxis) : 0;
+        float curSpeedY = CanMove ? (isRunning ? RunningSpeed : WalkingSpeed) * Input.GetAxis(StrafeAxis) : 0;
+
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Jump
+        if (Input.GetKey(JumpKey) && CanMove && characterController.isGrounded)
+            moveDirection.y = JumpSpeed;
+        else
+            moveDirection.y = movementDirectionY;
+
+        // Apply gravity
+        if (!characterController.isGrounded)
+            moveDirection.y -= Gravity * Time.deltaTime;
+    }
+}
