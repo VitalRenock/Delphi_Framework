bf60f549c0166431f532352242394acf0ee5a3f4
VitalRenock <55354524+VitalRenock@users.noreply.github.com>
2020-07-25 19:57:32 +0200

    Update TerrainGenerator

---
+++ Assets/Delphi/Testrota.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testrota : MonoBehaviour
+{
+    public float Angle = 0f;
+    public Vector2 Old = Vector2.zero;
+    [ShowInInspector][ReadOnly]
+    Vector2 New = Vector2.zero;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(Old, 0.1f);
+
+
+        float newX = (Old.x * Mathf.Cos(Angle)) + (Old.y * Mathf.Sin(Angle));
+        float newY = (Old.x * Mathf.Sin(Angle)) + (Old.y * Mathf.Cos(Angle));
+        New = new Vector2(newX, newY);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(New, 0.1f);
+    }
+}
